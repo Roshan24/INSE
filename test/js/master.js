78a9a25eabaad20a1382669356f69c2c418e3798
Josh <up847988@myport.ac.uk>
2019-02-08 12:24:42 +0000

    Refactoring, moved login specific js to a new specific file

--- test/js/master.js
+++ test/js/master.js
@@ -9,38 +9,22 @@ window.addEventListener("load", () => {
     handleScroll(e);
   });
 
-  //Special page load for login.html
-  const page = window.location.href.slice(window.location.href.lastIndexOf("/", window.location.href.length));
-  if (page === "/login.html") checkLoginSignUp();
+  const login = document.getElementById("login");
+  login.addEventListener("click", () => {
+    localStorage.setItem("login", "true");
+  });
 
-  const confirmPassword = document.getElementById("confirm-password");
-  if (confirmPassword) {
-    confirmPassword.addEventListener("input", (e) => {
-      checkConfirmPasswordValid(e.target);
-    });
-  }
-
-  const password = document.getElementById("password");
-  if (password) {
-    password.addEventListener("input", (e) => {
-      checkPasswordValid(e.target);
-      checkConfirmPasswordValid(confirmPassword);
-    })
-  }
-
-  const submit = document.getElementById("submit");
-  if (submit) {
-    submit.addEventListener("click", (e) => {
-      handleSubmit(e.target);
-    })
-  }
+  const signup = document.getElementById("signup");
+  signup.addEventListener("click", () => {
+    localStorage.setItem("login", "false");
+  });
 
 });
 
 //Loads and styles the navigtion bar
 function loadNavBar() {
     const nav = document.getElementById("nav");
-    nav.innerHTML = '<a href="index.html" class="active">Home</a><a href="sport.html">Sports</a><a href="league.html">Leagues</a><a href="about-us.html">About Us</a><span id="nav-login"><i class="material-icons" id="account">account_circle</i><a href="login.html" id="login" onclick="localStorage.setItem(\'login\', \'true\')">Login</a> or <a href="login.html" id="signup" onclick="localStorage.setItem(\'login\', \'false\')">Sign up</a></span>';
+    nav.innerHTML = '<a href="index.html" class="active">Home</a><a href="sport.html">Sports</a><a href="league.html">Leagues</a><a href="about-us.html">About Us</a><span id="nav-login"><i class="material-icons" id="account">account_circle</i><a href="login.html" id="login">Login</a> or <a href="login.html" id="signup">Sign up</a></span>';
 
     setActive(window.location.href, nav);
 }
@@ -92,9 +76,9 @@ function checkLoggedIn() {
 
   if (localStorage.getItem("loggedIn") == undefined) {
     localStorage.setItem("loggedIn", "false");
-    navLogin.innerHTML = '<a href="login.html" id="login" onclick="localStorage.setItem(\'login\', \'true\')">Login</a> or <a href="login.html" id="signup" onclick="localStorage.setItem(\'login\', \'false\')">Sign up</a>'
+    navLogin.innerHTML = '<a href="login.html">Login</a> or <a href="login.html" id="signup">Sign up</a>'
   } else if (localStorage.getItem("loggedIn") == "false") {
-    navLogin.innerHTML = '<a href="login.html" id="login" onclick="localStorage.setItem(\'login\', \'true\')">Login</a> or <a href="login.html" id="signup" onclick="localStorage.setItem(\'login\', \'false\')">Sign up</a>'
+    navLogin.innerHTML = '<a href="login.html" id="login">Login</a> or <a href="login.html" id="signup">Sign up</a>'
   } else if (localStorage.getItem("loggedIn") == "true") {
     navLogin.innerHTML = '<i class="material-icons" id="account">account_circle</i>'
 
@@ -106,78 +90,7 @@ function checkLoggedIn() {
 
 }
 
-// TODO: Complete this function. Will handle the user
-// logging in/signing up
-function handleSubmit(e) {
-  if (e.textContent === "Login") {
-    // postDataToServer('profile.html', {
-    //   // username:
-    // });
-  } else if (e.textContent === "Sign Up") {
-
-  } else {
-    console.log(e.textContent);
-    throw new Error("Login Error");
-  }
-}
-
 // TODO: Will redirect user to their personal profile
 function handleAccountBtnClicked() {
 
-}
-
-
-/**
- * Sends a post request to the server.
- *
- * @param url            url post request is sent to
- *
- * @param data           data sent with the post request *                       to url in JSON format
- *
- * @return jsonResponse  JSON response from the server
- */
-async function postDataToServer(url = ``, data = {}) {
-  const response = fetch(url, {
-    method: "POST",
-    mode: "same-origin",
-    cache: "no-cache",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  });
-  const jsonResponse = response.json();
-  return jsonResponse;
-}
-
-//Styles login.html depending on whether user is logging in or not
-function checkLoginSignUp() {
-  if (localStorage.getItem("login") === "true") {
-    document.getElementById("confirm-password-label").className = "hidden";
-    document.getElementById("email-label").style.top = "25%";
-    document.getElementById("password-label").style.top = "50%";
-    localStorage.setItem("login", "");
-    document.getElementById("submit").textContent = "Login";
-  } else if (localStorage.getItem("login") === "false") {
-    document.getElementById("confirm-password-label").className = "";
-    localStorage.setItem("login", "");
-    document.getElementById("submit").textContent = "Sign Up";
-  }
-}
-
-function checkPasswordValid(e) {
-  const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,50}$/g;
-  const verification = regex.test(e.value);
-
-  e.style.borderBottomColor = (verification)?"#2F8":"#F11";
-}
-
-function checkConfirmPasswordValid(e) {
-  if (!e) return false;
-  if (e.value === "") return false;
-
-  const passwordElem = document.getElementById("password");
-  const password = passwordElem.value;
-
-  e.style.borderBottomColor = (e.value === password)?"#2F8":"#F11";
 }
