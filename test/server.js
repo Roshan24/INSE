946946f345f89af5eb2578724d2f17b7afd2d5e5
Josh <up847988@myport.ac.uk>
2019-03-20 14:41:58 +0000

    fixing file problem

---
+++ test/server.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.redirect('./index.html');
+});
+
+app.use(express.static(__dirname));
+
+app.post('/profile.html', (req, res) => {
+    res.redirect('./profile.html');
+    console.log("It worked!");
+});
+
+app.listen(8080);
