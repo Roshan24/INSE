427ac8d6f28cc49ace075065c568c335121dfa88
Josh <up847988@myport.ac.uk>
2019-03-21 13:19:03 +0000

    database first test

--- test/server.js
+++ test/server.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const app = express();
+const mysql = require('mysql2');
 
 app.get('/', (req, res) => {
     res.redirect('./index.html');
@@ -14,6 +15,23 @@ app.post('/profile.html', (req, res) => {
     console.log("It worked!");
 });
 
+app.post('/teamJoin.html', (req, res) => {
+    return getTeams();
+})
 
+async function getTeams() {
+    const sql = await init();
+    const query = 'SELECT * FROM team';
+
+    const [results] = await sql.query(query);
+    return results;
+}
+
+async function init() {
+    if (sqlPromise) return sqlPromise;
+
+    sqlPromise = newConnection();
+    return sqlPromise;
+}
 
 app.listen(8080);
