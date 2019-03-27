98c3c00cab33eebb5fbfd3bdee76383e08e1d087
Chris D'Sa <chrisdsa99@gmail.com>
2019-03-27 14:26:08 +0000

    Created SQL tables

--- test/init_sql.sql
+++ test/init_sql.sql
@@ -1,7 +1,26 @@
+-- create database if not exists sportvantage;
+--
+-- create table if not exists sportvantage.team (
+--     test varchar(20)
+-- ) charset 'utf8mb4';
+--
+-- insert into sportvantage.team values ('test');
+
 create database if not exists sportvantage;
+  create table teams(
+    teamID      INT           AUTO_INCREMENT PRIMARY KEY
+    sportID     INT           NOT NULL
+    leagueID    INT           NOT NULL
+    playerID    INT           NOT NULL
+    reqPlayers  INT           NOT NULL
+    CONSTRAINT FK_sport FOREIGN KEY (sportID) REFERENCES sport(sportID),
+    CONSTRAINT FK_league FOREIGN KEY (leagueID) REFERENCES league(leagueID),
+    CONSTRAINT FK_players FOREIGN KEY (playerID) REFERENCES players(playerID)
+  );
 
-create table if not exists sportvantage.team (
-    test varchar(20)
-) charset 'utf8mb4';
-
-insert into sportvantage.team values ('test');
+  create table if not exists sport(
+    sportID     INT           AUTO_INCREMENT PRIMARY KEY
+    sportName   CHAR(10)      NOT NULL
+  );
+  create table if not exists league();
+  create table if not exists users() charset 'utf8mb4';
